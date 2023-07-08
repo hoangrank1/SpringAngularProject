7a90ef6c67fcdfef26ed3c1e605434b196bc8cc3
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-07-08 17:18:59 +0700

    ping status

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -31,6 +31,7 @@ export class AppComponent {
     this.appState$ = this.serverService.servers$
       .pipe(
         map(response => {
+          this.dataSubject.next(response);
           return { 
             dataState: DataState.LOADED_STATE, 
             appData: response
@@ -40,6 +41,24 @@ export class AppComponent {
           dataState: DataState.LOADING_STATE 
         }),
         catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+        map(response => {
+          const index = this.dataSubject.value.data.servers.findIndex(server =>  server.id === response.data.server.id);
+          this.dataSubject.value.data.servers[index] = response.data.server;
+          this.filterSubject.next('');
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.filterSubject.next('');
           return of({ dataState: DataState.ERROR_STATE, error });
         })
       );
