4f1c429b412ac5bfb9fb10f2c43c19c6a8b0e017
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-07-08 16:57:35 +0700

    Server Injection

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { Server } from './interface/server';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoading.asObservable();
+
+
+  constructor(private serverService: ServerService) { }
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => {
+          return { 
+            dataState: DataState.LOADED_STATE, 
+            appData: response
+          }
+        }),
+        startWith({ 
+          dataState: DataState.LOADING_STATE 
+        }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
 }
